1f2fae6aa5b2a7ed328c8b2a92ef37224c88f0d1
Katrin Leinweber <kleinweber@gitlab.com>
2021-06-15 16:05:56 +0200

    Refactor as suggested by…
    https://til.hashrocket.com/posts/f4agttd8si-chaining-then-in-ruby-26

--- gitlab-support/0.1.0/reference.rb
+++ gitlab-support/0.1.0/reference.rb
@@ -14,19 +14,19 @@ class Reference
     return '' if url.nil? || url.empty?
 
     url
-      .sub(%r{^https?://git(la|hu)b.com}, '') # Remove domain
-      .sub('/groups', '')                     # Remove entity prefix (for non-project epics)
-      .sub(%r{^/gitlab-(com|org)}, '')        # Remove unnecessary top-level groups
-      .sub(%r{[/\\]?#[\w-]+$}, '')            # Remove anchor text
-      .sub(%r{[/\\](diff|design).*$}, '')     # Remove sub-entities
-      .sub('/support', '')                    #   and sub-level group names
-      .sub('/-', '')                          # Remove routing separator (gitlab#214217)
-      .sub('/epics/',          '&')           # Replace
-      .sub('/issues/',         '#')           #   type
-      .sub('/merge_requests/', '!')           #   with
-      .sub('/milestones/',     '%')           #   symbol
-      .sub(%r{/+}, '/')                       # Remove additional,
-      .delete_prefix('/')                     #   superfluous slashes
+      .then { |u| u.sub(%r{^https?://git(la|hu)b.com}, '') } # Remove domain
+      .then { |u| u.sub('/groups', '') }                     # Remove entity prefix (for non-project epics)
+      .then { |u| u.sub(%r{^/gitlab-(com|org)}, '') }        # Remove unnecessary top-level groups
+      .then { |u| u.sub(%r{[/\\]?#[\w-]+$}, '') }            # Remove anchor text
+      .then { |u| u.sub(%r{[/\\](diff|design).*$}, '') }     # Remove sub-entities
+      .then { |u| u.sub('/support', '') }                    #   and sub-level group names
+      .then { |u| u.sub('/-', '') }                          # Remove routing separator (gitlab#214217)
+      .then { |u| u.sub('/epics/',          '&') }           # Replace
+      .then { |u| u.sub('/issues/',         '#') }           #   type
+      .then { |u| u.sub('/merge_requests/', '!') }           #   with
+      .then { |u| u.sub('/milestones/',     '%') }           #   symbol
+      .then { |u| u.sub(%r{/+}, '/') }                       # Remove additional,
+      .then { |u| u.delete_prefix('/') }                     #   superfluous slashes
   end
 end
 
